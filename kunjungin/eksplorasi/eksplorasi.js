fa5790d2d4bfda3784348dacdc5e203fa6cbb0ff
reymundadwi <reymunda613@gmail.com>
2021-07-28 02:20:17 +0700

    Add animation menu

--- kunjungin/eksplorasi/eksplorasi.js
+++ kunjungin/eksplorasi/eksplorasi.js
@@ -1,20 +1,30 @@
-let h1 = document.querySelector('.main h1');
+let h1 = document.querySelector('.main h1'),
+    menu = document.querySelectorAll('.col-md-2 div');
 h1.style.opacity = 0;
 h1.style.transition = '1s';
+menu.forEach(e => {
+    e.style.opacity = 0;
+    e.style.transition = '1s';
+})
+
+
 window.addEventListener('load',() =>{
     h1.style.opacity = 1;
+    menu.forEach(e => {
+        e.style.opacity = 1;
+    })
     setTimeout(() => {
         h1.style.opacity = 0;
-    },4000)
+    },3000)
     setTimeout(() => {
         h1.textContent = 'Silakan pilih salah satu fitur ini!';
         h1.style.opacity = 1;
-    },5000)
+    },4000)
     setTimeout(() => {
         h1.style.opacity = 0;
-    },8000)
+    },7000)
     setTimeout(() => {
         h1.textContent = 'Semoga membantu ^_^';
         h1.style.opacity = 1;
-    },9000)
+    },8000)
 })
